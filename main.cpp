c08af3df89497356fb2fd4ad845f777e3a079882
Doji <luff.zz1011@gmail.com>
2022-10-04 18:03:53 +0800

    Lesson 3 Z Buffer
    Add z buffer to fix the mouth problem, though there still exists some
    annoying black pixels among the triangles, I will try to fix this bug in
    a new branch.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor blue  = TGAColor(0,   0,   255, 255);
 Model *model = NULL;
 const int width = 800;
 const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color) { 
+    int x0 = v0.x, y0 = v0.y, x1 = v1.x, y1 = v1.y;
     bool steep = false;
     if(std::abs(x0-x1) < std::abs(y0-y1)){
         // always iterate through x axis
@@ -49,8 +51,19 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
+Vec3f barycentric(const Vec3i &v0, const Vec3i &v1, const Vec3i &v2, const Vec2i &p){
+    // no need to get confused by math, just use!
+    Vec3f vector1(v0.x - p.x, v1.x - v0.x, v2.x - v0.x);
+    Vec3f vector2(v0.y - p.y, v1.y - v0.y, v2.y - v0.y);
+
+    Vec3f vector3 = vector1 ^ vector2;
+    vector3 = vector3 * (1 / vector3.x);
+    vector3.x = 1 - vector3.y - vector3.z;
+    return vector3;
+}
+
 // draw the triangle using barycentric coordinate
-void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color){
+void triangle(Vec3i v0, Vec3i v1, Vec3i v2, TGAImage &image, TGAColor color, std::vector<float> &zbuffer){
     // get boouding box
     Vec2i maxV, minV;
     // max (x, y)
@@ -67,19 +80,17 @@ void triangle(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color){
     // traverse in bouding box
     for(int x = minV.x; x <= maxV.x; x++){
         for(int y = minV.y; y <= maxV.y; y++){
-            // if (x, y) in triangle, v0->A, v1->B, v2->C
-            Vec3f vector1(v0.x - x, v1.x - v0.x, v2.x - v0.x);
-            Vec3f vector2(v0.y - y, v1.y - v0.y, v2.y - v0.y);
+            Vec3f barycentric_coord = barycentric(v0, v1, v2, Vec2i(x, y));
 
-            // cross product to get (1, u, v)
-            Vec3f vector3 = vector1 ^ vector2;
-            // vector3.x = vector1.y * vector2.z - vector1.z * vector2.y;
-            // vector3.y = vector1.z * vector2.x - vector1.x * vector2.z;
-            // vector3.z = vector1.x * vector2.y - vector1.y * vector2.x;
-            vector3 = vector3 * (1 / vector3.x);
-
-            if(vector3.y >=0 && vector3.z >= 0 && vector3.y + vector3.z <= 1)
-                image.set(x, y, color);
+            // pixel in the triangle
+            if(barycentric_coord.y >=0 && barycentric_coord.z >= 0 && barycentric_coord.x >= 0){
+                // the depth 
+                float z = barycentric_coord.y * v1.z + barycentric_coord.z * v2.z + barycentric_coord.x * v0.z;
+                if(z > zbuffer[x * width + y]){
+                    image.set(x, y, color);
+                    zbuffer[x * width + y] = z;
+                }
+            }
         }
     }
 }
@@ -93,22 +104,18 @@ int main(int argc, char** argv) {
         model = new Model("obj/african_head.obj");
 
     TGAImage image(width, height, TGAImage::RGB);
+    // never mind, for depth comparison, we do not care the precision, int is enough
+    std::vector<float> zbuffer(width * height, -1000);
     Vec3f light_dir(0, 0, -1);
-    
-    // Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    // Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    // Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-    // triangle(t0[0], t0[1], t0[2], image, white);
-    // triangle(t1[0], t1[1], t1[2], image, green);
-    // triangle(t2[0], t2[1], t2[2], image, red);
+
     for(int i=0; i < model->nfaces(); i++){
         // fetch point data
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
+        Vec3i screen_coords[3];
         Vec3f world_coords[3];
         for(int j=0; j<3; j++){
             world_coords[j] = model->vert(face[j]);
-            screen_coords[j] = Vec2i((world_coords[j].x + 1.)*width/2., (world_coords[j].y+1.)*height/2.);
+            screen_coords[j] = Vec3i((world_coords[j].x + 1.)*width/2., (world_coords[j].y+1.)*height/2., (int)world_coords[j].z);
         }
 
         // calculate light intensity
@@ -119,7 +126,7 @@ int main(int argc, char** argv) {
         float intensity = normal * light_dir;
         if(intensity > 0){
             TGAColor c(255 * intensity, 255 * intensity, 255 * intensity, 255);
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, c);
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, c, zbuffer);
         }
     }
 
